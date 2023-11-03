312d3958bbf030769f98e76b6fcf1f0d01e9da15
Marcos Sebastiao <m.juniorcsebastiao@gmail.com>
2023-11-03 17:26:00 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+/**
+ * @format
+ */
+
+import 'react-native-get-random-values';
+import React from 'react';
+import {AppRegistry} from 'react-native';
+import {AppWrapperNonSync} from './app/AppWrapperNonSync';
+import {AppWrapperSync} from './app/AppWrapperSync';
+import {name as appName} from './app.json';
+import {SYNC_CONFIG} from './sync.config';
+
+const App = () =>
+  SYNC_CONFIG.enabled ? (
+    <AppWrapperSync appId={SYNC_CONFIG.appId} />
+  ) : (
+    <AppWrapperNonSync />
+  );
+
+export default App;
+
+AppRegistry.registerComponent(appName, () => App);
